85cddc003e13d7ffb1e7625b7d191db0f10906d0
Ankrishx20 <ankrishkandpal1996@gmail.com>
2019-06-27 21:24:12 +0530

    Updates

---
+++ precache-manifest.5be344b3239e96dfcad90423ad870bf6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5d6519d5524cc26bcdd180062c74a0a",
+    "url": "/reactcalculator/index.html"
+  },
+  {
+    "revision": "364baf8be41410bc62c4",
+    "url": "/reactcalculator/static/css/main.7ee964ee.chunk.css"
+  },
+  {
+    "revision": "efc3d230fd1e1b3caa34",
+    "url": "/reactcalculator/static/js/2.bbcd3f81.chunk.js"
+  },
+  {
+    "revision": "364baf8be41410bc62c4",
+    "url": "/reactcalculator/static/js/main.d29c38c2.chunk.js"
+  },
+  {
+    "revision": "72f3015dd0be49f3d573",
+    "url": "/reactcalculator/static/js/runtime~main.57313a7a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactcalculator/static/media/logo.5d5d9eef.svg"
+  }
+]);
